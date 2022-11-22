d3cb61f07912bc305ddae832d0902eb83a5a8933
MarcosSouzaa <marcossouza.git@gmail.com>
2022-11-22 16:58:45 -0300

    Date select

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SalesRepository.java
@@ -1,9 +1,21 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
 public interface SalesRepository extends JpaRepository<Sale, Long> {
+	
+	//vou incluir uma função que faz uma pesquisa no banco de dados recebendo as datas como argumentos
+	
+	//linguagem JPQO
+	
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -14,8 +18,22 @@ public class SaleService {
 	@Autowired
 	private SalesRepository repository; //para acessar o banco de dados
 	
-	public List<Sale> findSales() { //retorna uma lista de vendas
-		return repository.findAll(); //return = o resultado da minha função é o resultado que o repository vai buscar
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) { //retorna uma lista de vendas no primeiro momento e depois mudo pra Page
+		
+		//cria uma data para o dia de hoje
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//vamos converter a data que está sendo passada com texto pelo argumento para localDate
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate); //data mínima um ano atrás
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		// ? se a data max for igual vazio vou colocar today no lugar : se não for verdade, converto para localDate
+		
+		return repository.findSales(min, max, pageable); //return = o resultado da minha função é o resultado que o repository vai buscar
+		
+		
+		
+	
 	}
 
 }

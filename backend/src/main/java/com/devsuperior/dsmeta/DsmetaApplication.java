8f0116620962538811fb70b28e1d9b56dc9d80b5
MarcosSouzaa <marcossouza.git@gmail.com>
2022-11-21 21:34:45 -0300

    Security config

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -1,13 +1,13 @@
-package com.devsuperior.devmeta;
+package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevmetaApplication {
+public class DsmetaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevmetaApplication.class, args);
+		SpringApplication.run(DsmetaApplication.class, args);
 	}
 
 }
